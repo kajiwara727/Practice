cd92f155760980fbed029dc4a1bc0ba141f2f784
YUUKAJI727\yuuka <yuukajiyan0727@gmail.com>
2024-09-11 03:58:05 +0900

    Webテキストにフォント適用

--- src/pages/memo_list.js
+++ src/pages/memo_list.js
@@ -1,9 +1,9 @@
-// pages/index.js
+
 export default function MemoLink() {
-  return (
-    <div>
-      <h1>シェアっと！</h1>
-      <p>メッセージ</p>
-    </div>
-  );
-}
+    return (
+      <div style={{ fontFamily: 'GenSenMaruGothic, sans-serif' }}>
+        <h1>シェアっと！</h1>
+        <p>メッセージ</p>
+      </div>
+    );
+  }

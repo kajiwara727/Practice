3feeac3d4e71aab4c803e8d22c9739ab73da6def
YUUKAJI727\yuuka <yuukajiyan0727@gmail.com>
2024-09-12 20:26:38 +0900

    no message

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/fonts.css'; // グローバルCSSをインポート
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- src/pages/memo_list.js
+++
@@ -1,9 +0,0 @@
-
-export default function MemoLink() {
-    return (
-      <div style={{ fontFamily: 'GenSenMaruGothic, sans-serif' }}>
-        <h1>シェアっと！</h1>
-        <p>メッセージ</p>
-      </div>
-    );
-  }
